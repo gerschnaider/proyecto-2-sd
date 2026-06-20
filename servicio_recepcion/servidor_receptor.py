f8836506e3147e811d4b4bc9d92e40200062a39b
Sabrina <sabribarrion@gmail.com>
2026-06-20 11:53:24 -0300

    Cargar en la base de datos

--- servicio_recepcion/servidor_receptor.py
+++ servicio_recepcion/servidor_receptor.py
@@ -5,7 +5,6 @@ import noticias_pb2_grpc
 
 class ReceptorNoticiasServicer(noticias_pb2_grpc.ReceptorNoticiasServicer):
     
-    
     def EnviarNoticia(self, request, context):
         print(f"\n[+] ¡Llegó una nueva noticia!")
         print(f"ID Noticia: {request.id_noticia}")
@@ -17,11 +16,43 @@ class ReceptorNoticiasServicer(noticias_pb2_grpc.ReceptorNoticiasServicer):
         
         # Pendiente: Conectar y guardar en la base de datos centralizada
         print("-> Guardando en la base de datos...")
+        nuevo_id = None
+        try:
+            # Establecemos la conexión (traductor psycopg2)
+            conexion = psycopg2.connect(
+                host="localhost",       # Cambiará cuando se use la red de Docker Swarm
+                database="noticias_db", # Nombre de la base de datos compartida
+                user="postgres",        # Usuario por defecto de Postgres
+                password="admin"        # Contraseña local de desarrollo
+            )
+            cursor = conexion.cursor()
+            
+            # Consulta SQL
+            insert_query = """
+                INSERT INTO news (title, user_id, category_id, content)
+                VALUES (%s, %s, %s, %s)
+                RETURNING news_id;
+            """
+            datos_a_insertar = (request.titulo, request.id_autor, request.id_categoria, request.texto)
+            
+            cursor.execute(insert_query, datos_a_insertar)
+            nuevo_id = cursor.fetchone()[0] # Atrapamos el ID autoincremental (SERIAL) generado por Postgres
+            
+            conexion.commit()
+            cursor.close()
+            conexion.close()
+            print(f"-> [ÉXITO] Noticia guardada correctamente con ID: {nuevo_id}")
+
+        except Exception as e:
+            print(f"-> [ERROR] No se pudo guardar en la base de datos. Detalle: {e}")
+            # print("-> [MODO DESARROLLO] Generando un ID temporal para continuar la simulación...")
+            # nuevo_id = 9999 # ID de respaldo para que el servicio de streaming no se quede sin dato
         
+
         # Pendiente: Hacer llamado gRPC al segundo servicio (Gonzalo_A)
         print("-> Avisando al servicio de distribución de noticias...")
 
-        # Responder al balanceador o cliente que todo salió perfecto
+        # Responder al cliente que todo salió bien
         return noticias_pb2.Respuesta(
             exito=True, 
             mensaje="La noticia fue recibida y guardada exitosamente."
